ef473995fb622da704cc2b8d11a01c5efdf043be
pav1222 <pav333zt@gmail.com>
2024-05-26 11:39:58 +0300

    b1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,42 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
-    void clear() {
+     void clear() {
+        Arrays.fill(storage, null);
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+
     }
 
     Resume get(String uuid) {
+        if(storage[0].uuid.equals(uuid)) {
+            return storage[0];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        storage[4] = null;
+        for (int i = 2;i < storage.length;i++) {
+            storage[i - 1] = storage[i];
+            storage[i] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage,storage.length);
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }

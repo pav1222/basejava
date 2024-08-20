d782a40925d92fb4fe683d40f133f007c2c4359f
pav1222 <pav333zt@gmail.com>
2024-08-20 15:33:51 +0300

    BD_2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,55 +5,68 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
 
     int size = 0;
 
-    public void update(Resume resume) {
+    int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(resume.uuid)) {
-                storage[i] = resume;
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
             }
         }
+        return -1;
     }
+    public void update(Resume resume) {
+        int index = getIndex(resume.uuid);
+        if (index != -1) {
+            storage[index] = resume;
+        } else {
+            System.out.println("Error.Resume not found");
+            }
+        }
+
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
-
     }
 
     void save(Resume resume) {
+        int index = getIndex(resume.uuid);
         if (storage.length > size) {
             System.out.println("Error.Array overflow");
         }
-        storage[size] = resume;
+        storage[index] = resume;
         size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
+        int index = getIndex(uuid);
+        if (index != -1) {
+            return storage[index];
             }
-        }
         return null;
-    }
+        }
+
+
 
     void delete(String uuid) {
+        int index = getIndex(uuid);
+            if (index == -1) {
+                System.out.println("Resume not found");
+                return;
+            }
 
-        for (int i = 0; i < size; i++) {
-
-            if (storage[i].uuid.equals(uuid)) {
-                size--;
-                if (i == size) {
-                    storage[i] = null;
-                } else {
-                    storage[i] = storage[size];
-                }
-                break;
+            size--;
+            if (index == size) {
+                storage[index] = null;
+            } else {
+                storage[index] = storage[size];
             }
         }
-    }
+
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
